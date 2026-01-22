e11a38a0e17ed4222a1d6a722f6c54f4b53e1490
gabriele.previtera <gabriele.previtera@epsilonline.com>
2026-01-22 17:51:15 +0100

    Fix: missing methed in ClusterFactory

--- aws_swiffer/factory/ecs/ClusterFactory.py
+++ aws_swiffer/factory/ecs/ClusterFactory.py
@@ -32,3 +32,6 @@ class ClusterFactory(IFactory):
 
     def create_by_id(self, resource_id: str) -> IResource:
         return self.create_by_name(resource_id)
+
+    def create_by_list_file() -> list[IResource]:
+        raise NotImplementedError
